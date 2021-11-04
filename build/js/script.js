fbad37bfce3bbde0a862177040a0fd872392641a
Anna Tolstikova <39711434+Asusey@users.noreply.github.com>
2021-11-05 01:16:01 +0300

    Поменяла изображения, подключила скрипт

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,39 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
+  var input = document.getElementById('phone');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  input.addEventListener('keypress', function (evt) {
+    if (evt.keyCode < 47 || evt.keyCode > 57) {
+      evt.preventDefault();
+    }
+  });
+
+  input.addEventListener('focus', function () {
+    if (input.value.length === 0) {
+      input.value = '+7(';
+      input.selectionStart = input.value.length;
+    }
+  });
+
+  input.addEventListener('click', function (evt) {
+    if (input.selectionStart < 3) {
+      input.selectionStart = input.value.length;
+    }
+    if (evt.key === 'Backspace' && input.value.length <= 3) {
+      evt.preventDefault();
+    }
+  });
+
+  input.addEventListener('blur', function () {
+    if (input.value === '+7(') {
+      input.value = '';
+    }
+  });
+
+  input.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Backspace' && input.value.length <= 3) {
+      evt.preventDefault();
+    }
+  });
+})();
